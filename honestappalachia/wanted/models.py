556f8f66f589415834ed8dac32547e1b9274668d
Garrett Robinson <garrett.f.robinson@gmail.com>
2012-01-20 10:52:48 -0500

    Basic Story model

--- honestappalachia/wanted/models.py
+++ honestappalachia/wanted/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Story(models.Model):
+    '''
+    A text description and associated location
+    '''
+    description = models.CharField(max_length=140)
+    location = models.CharField(max_length=140)
+
+    created = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.description[:20] + "..."
